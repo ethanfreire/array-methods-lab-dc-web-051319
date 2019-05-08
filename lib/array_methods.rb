9fe7366f6fabf846cc48b34b7a7fcc166cc3011e
Your Name <you@example.com>
2019-05-08 06:03:04 -0400

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,30 @@
 def using_include(array, element)
+  array.include?(element)
+  #boolean
 
 end
 
 def using_sort(array)
-
+  array.sort!
+#sorted in ascending order
 end
 
 def using_reverse(array)
-
+  array.reverse
+#return array in reverse order
 end
 
 def using_first(array)
-
+  array.first
+#return first element in array
 end
 
 def using_last(array)
-
+  array.last
+#return last element in array
 end
 
 def using_size(array)
-
+  array.size
+#return int or the size of the array
 end
